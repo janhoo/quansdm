1fea68e07fc647c86a693123b4b3911ccbcc02d9
janhoo <holstein@uni-bremen.de>
2015-02-12 13:10:31 +0100

    initial commit

--- R/quansdm.R
+++ R/quansdm.R
@@ -1,5 +1,15 @@
-#' Generate R documentation from inline comments.
-#' @title quansdm
+#' quansdm: quantitative species distribution modelling framework
+#' 
+#' quansdm provides wrapper functions for some popular species distribution modelling methods.
+#' 
+#' It has three main goals:
+#' \itemize{
+#' \item Free you from tedious tasks.
+#' \item Provide a large variety of model performance metrics.
+#' \item Provide model validation techniques like cross-validation, null-models, 
+#' and  exploration of spatial autocorrelation.
+#' }
 #' @docType package
 #' @name quansdm
+#' @useDynLib quansdm
 NULL
